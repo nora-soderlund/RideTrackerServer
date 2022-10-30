b08c5c5a13825fce75802ce549dd4255ef86a65c
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-10-30 18:09:29 +0100

    Fixed multiple sections in 3d playback

--- app/Public/Playback/scripts/Playback.js
+++ app/Public/Playback/scripts/Playback.js
@@ -1,29 +1,6 @@
-// Converts from degrees to radians.
-function toRadians(degrees) {
-    return degrees * Math.PI / 180;
-  };
-   
-  // Converts from radians to degrees.
-  function toDegrees(radians) {
-    return radians * 180 / Math.PI;
-  }
-  
-  
-  function bearing(startLat, startLng, destLat, destLng){
-    startLat = toRadians(startLat);
-    startLng = toRadians(startLng);
-    destLat = toRadians(destLat);
-    destLng = toRadians(destLng);
-  
-    y = Math.sin(destLng - startLng) * Math.cos(destLat);
-    x = Math.cos(startLat) * Math.sin(destLat) -
-          Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
-    brng = Math.atan2(y, x);
-    brng = toDegrees(brng);
-    return (brng + 360) % 360;
-  }
-
 class Playback {
+    static startIndex = 1;
+
     constructor(element, activity, color, background) {
         this.element = element;
         this.activity = activity;
@@ -146,8 +123,8 @@ class Playback {
 
             this.map.setZoom(16);
             this.map.setTilt(90);
-            this.map.panTo(new google.maps.LatLng(section.coordinates[20].coords.latitude, section.coordinates[20].coords.longitude));
-            //this.map.setHeading(section.coordinates[20].coords.heading);
+            this.map.panTo(new google.maps.LatLng(section.coordinates[Playback.startIndex].coords.latitude, section.coordinates[Playback.startIndex].coords.longitude));
+            this.map.setHeading(section.coordinates[Playback.startIndex].coords.heading);
         });
     };
 
@@ -164,29 +141,44 @@ class Playback {
     };
 
     playback() {
-        let ready = true;
-        const section = this.data.sections[0];
-        const start = performance.now();
-        let delays = 0, timestamp = null;
-        let previousCoordinateIndex = null;
-        let heading = 0;
+        return new Promise((resolve) => {
+            let ready = true;
+            let sectionIndex = 0;
+            const start = performance.now();
+            let delays = 0, timestamp = null;
+            let previousCoordinateIndex = null;
+            let heading = 0;
 
-        this.map.addListener("idle", () => {
-            if(timestamp)
-                delays += performance.now() - timestamp;
-            
-            window.requestAnimationFrame(() => onMapRender());
-        });
-    
-        const onMapRender = () => {
-            const elapsed = ((performance.now() - start) - delays) * 60;
+            this.map.addListener("idle", () => {
+                if(timestamp)
+                    delays += performance.now() - timestamp;
+                
+                window.requestAnimationFrame(() => onMapRender());
+            });
+        
+            const onMapRender = () => {
+                const section = this.data.sections[sectionIndex];
+                
+                const elapsed = ((performance.now() - start) - delays) * 60;
 
-            const current = section.coordinates[20].timestamp + elapsed;
+                const current = section.coordinates[Playback.startIndex].timestamp + elapsed;
 
-            const index = section.coordinates.findIndex((coordinate) => coordinate.timestamp >= current);
-            const coordinate = section.coordinates[index];
+                const index = section.coordinates.findIndex((coordinate) => coordinate.timestamp >= current);
 
-            if(index - 1 >= 0) {
+                if(index == section.length - 1) {
+                    sectionIndex++;
+
+                    if(this.data.sections.length == sectionIndex) {
+                        google.maps.event.clearListeners(this.map, "idle");
+
+                        resolve();
+
+                        return;
+                    }
+                }
+
+                const coordinate = section.coordinates[index];
+
                 timestamp = null;
 
                 const previousCoordinate = section.coordinates[index - 1];
@@ -196,7 +188,7 @@ class Playback {
                 const difference = previousCoordinate.timestamp - coordinate.timestamp;
                 const differenceLatitude = previousCoordinate.coords.latitude - coordinate.coords.latitude;
                 const differenceLongitude = previousCoordinate.coords.longitude - coordinate.coords.longitude;
-                const differenceHeading = previousCoordinate.coords.heading - coordinate.coords.heading;
+                //const differenceHeading = previousCoordinate.coords.heading - coordinate.coords.heading;
 
                 const multiplier = duration / difference;
 
@@ -208,23 +200,6 @@ class Playback {
                 const camera = {
                     center
                 };
-
-                /*if(previousCoordinate.coords.speed >= 2 && coordinate.coords.speed >= 2) {
-                    let headingDifferences = 0;
-
-                    for(let headingIndex = index; headingIndex < index + 10; headingIndex++) {
-                        const headingCoordinate = section.coordinates[headingIndex];
-
-                        headingDifferences += coordinate.coords.heading - headingCoordinate.coords.heading;
-                    }
-
-                    console.log({
-                        headingDifferences
-                    });
-                    
-                    if(headingDifferences < 90)
-                        camera.heading = previousCoordinate.coords.heading + (differenceHeading * multiplier);
-                }*/
 
                 // let's iterate forwards in the coordinates array until we've gathered a difference of 20km/h, meaning we've _probably_ not stood still
                 // forever and we've advanced 5 meters. then use this coordinate's latitude and longitude to calculate the bearing from the currrent coordinate
@@ -238,29 +213,18 @@ class Playback {
                 while(bearingDifference < 20 / 3.6) {
                     bearingDifference += section.coordinates[bearingIndex].coords.speed;
 
+                    if(bearingIndex + 1 >= section.coordinates.length)
+                        break;
+
                     bearingIndex++;
                 }
 
                 let bearingCoordinate = section.coordinates[bearingIndex];
 
-                const bearingHeading = bearing(coordinate.coords.latitude, coordinate.coords.longitude, bearingCoordinate.coords.latitude, bearingCoordinate.coords.longitude);
+                const bearingHeading = this.bearing(coordinate.coords, bearingCoordinate.coords);
 
                 if(Math.abs(heading - bearingHeading) > 2)
                     camera.heading = heading = bearingHeading;
-
-                /*
-                
-
-                const headings = section.coordinates.splice(index - 5, 10).map((coordinate) => coordinate.coords.heading);
-                const headingSum = headings.reduce((a, b) => a + b, 0);
-                const headingAverage = (headingSum / headings.length) || 0;
-
-                if(heading == null)
-                    camera.heading = heading = headingAverage;
-                else if(previousCoordinate.coords.speed >= 2 && coordinate.coords.speed >= 2) {
-                    if(Math.abs(headingAverage - previousCoordinate.coords.heading) > 90)
-                        camera.heading = heading = headingAverage;
-                }*/
 
                 this.map.moveCamera(camera);
 
@@ -271,11 +235,31 @@ class Playback {
                 }
 
                 timestamp = performance.now();
-            }
-            else
-                window.requestAnimationFrame(() => onMapRender());
-        };
-    
-        window.requestAnimationFrame(() => onMapRender());
+            };
+        
+            window.requestAnimationFrame(() => onMapRender());
+        });
+    };
+
+    toRadians(degrees) {
+        return degrees * Math.PI / 180;
+    };
+
+    toDegrees(radians) {
+        return radians * 180 / Math.PI;
+    };
+
+    bearing(start, end) {
+        const startLatitude = this.toRadians(start.latitude);
+        const startLongitude = this.toRadians(start.longitude);
+        const endLatitude = this.toRadians(end.latitude);
+        const endLongitude = this.toRadians(end.longitude);
+
+        const y = Math.sin(endLongitude - startLongitude) * Math.cos(endLatitude);
+        const x = Math.cos(startLatitude) * Math.sin(endLatitude) - Math.sin(startLatitude) * Math.cos(endLatitude) * Math.cos(endLongitude - startLongitude);
+        
+        const bearing = this.toDegrees(Math.atan2(y, x));
+
+        return (bearing + 360) % 360;
     };
 };
