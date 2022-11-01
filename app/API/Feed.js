e95054d9c32b5bdc3ec2e5083654f948412d96ce
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-11-01 09:39:53 +0100

    Fixed feeding order

--- app/API/Feed.js
+++ app/API/Feed.js
@@ -4,7 +4,7 @@ import Database from "../Database.js";
 // this only sends the ids of the activities to show to allow for the
 // client to cache the activities it has already requested, if any.
 Server.on("GET", "/api/feed", async (request, response) => {
-    const rows = await Database.queryAsync("SELECT id FROM activities");
+    const rows = await Database.queryAsync("SELECT id FROM activities ORDER BY timestamp DESC");
 
     return {
         success: true,
