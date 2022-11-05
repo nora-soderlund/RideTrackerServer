bda5a6405523cfc2df4c736ad7420c3650b835a8
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-11-05 02:14:05 +0100

    Added bike sorting priority

--- app/API/User/Bikes.js
+++ app/API/User/Bikes.js
@@ -4,8 +4,19 @@ import Database from "../../Database.js";
 Server.on("GET", "/api/user/bikes", async (request, response, parameters) => {
     const rows = await Database.queryAsync(`SELECT id FROM bikes WHERE user = ${Database.connection.escape(parameters.user)}`);
 
+    let bikes = [];
+
+    for(let index = 0; index < rows.length; index++) {
+        const activities = await Database.queryAsync(`SELECT COUNT(id) AS rides FROM activities WHERE bike = ${Database.connection.escape(rows[index].id)}`);
+
+        bikes.push({
+            id: rows[index].id,
+            rides: activities[0].rides
+        });
+    }
+
     return {
         success: true,
-        content: rows.map((row) => row.id)
+        content: bikes.sort((a, b) => b.rides - a.rides).map((bike) => bike.id)
     };
 }, [ "user" ]);
