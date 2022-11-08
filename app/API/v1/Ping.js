8dbfc539eb0c0c9f5842d6879a13f7dc6e33b56f
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-11-08 12:58:14 +0100

    Added feed filters

--- app/API/v1/Ping.js
+++ app/API/v1/Ping.js
@@ -1,5 +1,5 @@
 import Server from "./../../Server.js";
 
-Server.on("GET", "/api/v1/v1/ping", async (request, response) => {
+Server.on("GET", "/api/v1/ping", async (request, response) => {
     return "pong!";
 }, null, "text/html");
