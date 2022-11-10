3912c58db8a6a427b7d3f7dafa3f6f1cd7fe8484
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-11-10 16:54:40 +0100

    Added MySQL fallbacks and self-diagnosing

--- app/Database.js
+++ app/Database.js
@@ -5,7 +5,9 @@ export default class Database {
 
     static connectAsync(settings) {
         return new Promise((resolve, reject) => {
-            this.connection = mysql.createConnection(settings);
+            this.settings = settings;
+
+            this.connection = mysql.createConnection(this.settings);
 
             this.connection.connect((error) => {
                 if(error)
@@ -18,12 +20,55 @@ export default class Database {
 
     static queryAsync(query) {
         return new Promise((resolve, reject) => {
-            this.connection.query(query, (error, rows) => {
-                if(error)
-                    return reject(error);
+            try {
+                this.connection.query(query, (error, rows) => {
+                    if(error)
+                        return reject(error);
+    
+                    return resolve(rows);
+                });
+            }
+            catch(error) {
+                console.error(`ERROR! MySQL query failed: ${error}`);
+                this.error("query", { query, error });
 
-                return resolve(rows);
-            });
+                this.diagnose();
+            }
         });
+    };
+
+    static diagnose() {
+        try {
+            console.warn("WARNING! Attempting to diagnose MySQL server state...");
+
+            const ping = connection.ping((error) => {
+                console.error(`ERROR: failed to talk with the MySQL server: ${error}`);
+                this.error("diagnose", { query, error });
+
+                console.warn(`WARNING! Attempting to restart the MySQL server connection...`);
+                this.connectAsync(this.settings).then(() => {
+                    this.diagnose();
+                }).catch((error) => {
+                    console.error(`ERROR! Couldn't reconnect to the MySQL server: ${error}`);
+                    this.error("diagnose", { error });
+                });
+            });
+
+            if(ping)
+                console.log("Ping to the MySQL server was succesful!");
+        }
+        catch(error) {
+            console.error(`ERROR! Diagnose failed: ${error}`);
+            this.error("diagnose", { error });
+        }
+    };
+
+    static error(category, object) {
+        const name = `mysql_${category}_${Date.now()}.json`;
+
+        if(!fs.existsSync("./logs/"))
+            fs.mkdirSync("./logs/");
+                
+        fs.writeFileSync(`./logs/${name}`, JSON.stringify(object));
     };
 };
